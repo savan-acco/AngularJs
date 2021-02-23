c2e1768ad4c9d2db26db30ab9b557a08998aa77d
Savan <savanlalakia@yahoo.in>
2021-02-23 23:26:46 +0530

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,23 +8,61 @@
  *
  * Main module of the application.
  */
-angular
-  .module('angularJsappApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+// angular
+//   .module('angularJsappApp', [
+//     'ngAnimate',
+//     'ngCookies',
+//     'ngResource',
+//     'ngRoute',
+//     'ngSanitize',
+//     'ngTouch'
+//   ])
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'views/main.html',
+//         controller: 'MainCtrl',
+//         controllerAs: 'main'
+//       })
+//       .otherwise({
+//         redirectTo: '/'
+//       });
+//   });
+
+  
+  var app = angular.module('myApp', ["ngRoute"]); 
+  app.config(function($routeProvider){
+      $routeProvider
+      .when("/",{
+          templateUrl:"todo.html",
+          controller: "todoCtrl"
+      })
+      .when("/new-york",{
+          templateUrl:"./../views/new-york.html",
+          controller:"city"
+      })
+      .when("/london",{
+          templateUrl:"./../views/london.html",
+          controller:"city"
       })
       .otherwise({
-        redirectTo: '/'
-      });
-  });
+          redirectTo:"/",
+      })
+  })
+
+    
+  app.service('siblingSharing',function(){
+  
+    list=[];
+
+    this.addToList=function(x)
+    {
+        list.push(x);
+    }
+
+    this.getList=function()
+    {
+        return list;
+    }
+
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,150 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name angularJsappApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the angularJsappApp
- */
-angular.module('angularJsappApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+// angular.module('angularJsappApp')
+//   .controller('MainCtrl', function () {
+//     this.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Karma'
+//     ];
+//   });
+
+
+var app = angular.module('myApp'); 
+  
+  app.component('firstComponent',{
+      template:'hello from first component ...{{$ctrl.works}}',
+      controller:function fc($scope)
+      {
+          this.works="it works";
+      }
+  });
+  
+  app.component('secondComponent',{
+      template:'hello from second component ...{{$ctrl.works}}',
+      controller:function fc($scope)
+      {
+          this.works="it works";
+      }
+  });
+  
+
+  app.service('siblingSharing',function(){
+  
+    let list=[];
+    
+    this.addToList=function(x)
+    {
+        list.push(x);
+    }
+
+    this.getList=function()
+    {
+        return list;
+    }
+
+});
+  
+  app.controller('sibling1',function($scope,siblingSharing){
+  
+      $scope.sibfunc=function(){
+          siblingSharing.addToList($scope.sibcon);
+          $scope.sibcon="";
+      }
+  
+  });
+  
+  app.controller('sibling2',function($scope,siblingSharing){
+  
+      $scope.list=[];
+      $scope.list=siblingSharing.getList();
+  
+  });
+  
+  
+  app.controller('city',function($scope){
+  
+  $scope.london="Controller Works...london";
+  $scope.ny="Controller Works...NY";
+  
+  })
+  
+  app.controller('todoCtrl', function($scope) {
+      $scope.todoList = [];
+  
+      $scope.todoAdd = function() {
+          $scope.todoList.push({todoText:$scope.todoInput,edit:false});
+          $scope.todoInput = "";
+      };
+  
+      $scope.edit=function(x)
+      {
+          x.edit=true;
+          //console.log($scope.todoList);
+      }
+  
+      $scope.makeChanges=function(x)
+      {
+          x.edit=false;
+          //console.log($scope.todoList);
+      }
+  
+      $scope.delete=function(x)
+      {
+          //console.log(x);
+          let ind=0;
+          $scope.todoList.forEach((val,iter)=>{
+              if(val==x)
+              {
+                  ind=iter;
+              }
+          })  
+          $scope.todoList.splice(ind,1);
+      }
+  });
+  
+  app.controller('serviceController',function($scope,$http)
+  {
+      
+      $scope.arr=[];
+      $http({
+          method : "GET",
+          url : "https://jsonplaceholder.typicode.com/todos"
+          })
+      .then(function(res){
+          
+      $scope.arr=res.data;    
+      $scope.arr.splice(10);
+      //console.log($scope.arr);
+      });
+  
+  
+  
+  });
+  
+  app.controller('level1controller',function($scope){
+  
+      $scope.$on('pass-up', function (event, args) {
+          $scope.v=args;
+      });
+  
+      $scope.broadcaster=function()
+      {
+          $scope.$broadcast('pass-down',$scope.txt1);
+      }
+  });
+  
+  
+  app.controller('level2controller',function($scope){
+  
+      $scope.$on('pass-down', function (event, args) {
+          $scope.v=args;
+      });
+  
+      $scope.emitter=function()
+      {
+          //console.log($scope.txt);
+      $scope.$emit('pass-up',$scope.txt2);
+      }
   });
